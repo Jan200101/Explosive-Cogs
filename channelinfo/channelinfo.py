1c4055fda0c008374d9273e135749c7b3e8c4878
Jan200101 <sentrycraft123@gmail.com>
2017-08-03 17:49:27 +0200

    Cleaned code

--- channelinfo/channelinfo.py
+++ channelinfo/channelinfo.py
@@ -1,7 +1,6 @@
+from random import randint, choice
 import discord
 from discord.ext import commands
-from .utils.chat_formatting import pagify, box
-from random import randint, choice
 
 class Channelinfo:
     """Shows Channel infos."""
@@ -22,14 +21,18 @@ class Channelinfo:
         else:
             userlist = "\n".join(userlist)
 
-        created_at = ("Created on {} ({} days ago)".format(channel.created_at.strftime("%d %b %Y %H:%M"), (ctx.message.timestamp - channel.created_at).days))
+        created_at = ("Created on {} ({} days ago)"
+                      "".format(channel.created_at.strftime("%d %b %Y %H:%M"),
+                                (ctx.message.timestamp - channel.created_at).days))
 
         emptyrand = u"\u2063" * randint(1, 10)
 
         colour = ''.join([choice('0123456789ABCDEF') for x in range(6)])
         colour = int(colour, 16)
 
-        data = discord.Embed(description="Channel ID: " + channel.id, colour=discord.Colour(value=colour))
+        data = discord.Embed(description="Channel ID: " + channel.id,
+                             colour=discord.Colour(value=colour))
+
         if "{}".format(channel.is_default) == "True":
             data.add_field(name="Default Channel", value="Yes")
         else:
@@ -41,7 +44,8 @@ class Channelinfo:
         if '{}'.format(channel.type) == "voice":
             if channel.user_limit != 0:
                 data.add_field(
-                    name="User Number", value="{}/{}".format(len(channel.voice_members), channel.user_limit))
+                    name="User Number",
+                    value="{}/{}".format(len(channel.voice_members), channel.user_limit))
             else:
                 data.add_field(name="User Number", value="{}".format(
                     len(channel.voice_members)))
