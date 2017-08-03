3ee8ea67a29fdd953aa56c59a3d57be619dd6701
Jan200101 <sentrycraft123@gmail.com>
2017-08-03 17:51:17 +0200

    Cleaned code

--- embednotification/embednotification.py
+++ embednotification/embednotification.py
@@ -1,9 +1,7 @@
-import discord
-from discord.ext import commands
-from cogs.utils import checks
-from cogs.utils.chat_formatting import box,  pagify, escape_mass_mentions
 from random import choice, randint
-import datetime
+from discord.ext import commands
+import discord
+from cogs.utils import checks
 
 class EmbedNotification:
     """Make announcements in embeds"""
@@ -13,21 +11,31 @@ class EmbedNotification:
 
     @commands.command(pass_context=True, no_pm=True, aliases=['embedm'])
     @checks.admin_or_permissions(administrator=True)
-    async def embednotification(self, ctx, text: str, color: str='000000'):
+    async def embednotification(self, ctx, text: str, color: str = '000000'):
         """Send a embed useful for announcements"""
 
-        if ctx.message.server.me.bot: # Check if user is a bot. If the user is bot there is no need to check for this
+        if ctx.message.server.me.bot:
             try:
                 await self.bot.delete_message(ctx.message)
             except:
-                await self.bot.send_message(ctx.message.author, 'I need the `Embed links` permission on `{}` to delete your message before it gets embeded.\n'
-                                                                'This is done to ensure the bots embed is in the same position your command was in.'.format(ctx.message.channel.mention))
+                await self.bot.send_message(ctx.message.author,
+                                            'I need the `Manage Messages` permission on `{}`'
+                                            'to delete your message before it gets embeded.\n'
+                                            'This is done to ensure the bots embed is in'
+                                            'the same position your command was in.'
+                                            ''.format(ctx.message.channel.mention))
                 return
         else:
             try:
                 await self.bot.delete_message(ctx.message)
             except:
-                await self.bot.send_message(ctx.message.server.get_member(self.bot.settings.owner), 'Your selfbot/This userbot is able to be used by others.\nThis is breaking Discords TOS and can be punished by them.\nThis messagte was send my embednotification.py')
+                await self.bot.send_message(ctx.message.server.get_member(self.bot.settings.owner),
+                                            'Your selfbot/This userbot is '
+                                            'able to be used by others.\n'
+                                            'This is breaking Discords TOS and'
+                                            ' can be punished by them.\n'
+                                            'This messagte was send my embednotification.py')
+
                 return
 
         color = color[:6]
@@ -35,15 +43,17 @@ class EmbedNotification:
         color = color.replace("0x", "")
         color = int(color, 16)
 
-        if not normaltext:
-            normaltext = u"\u2063" * randint(1, 10) # Generating a random number for a empty embed
+        normaltext = u"\u2063" * randint(1, 10) # Generating a random number for a empty embed
 
         data = discord.Embed(description=str(text), colour=discord.Colour(value=color))
 
         try:
             await self.bot.say(normaltext, embed=data)
         except:
-            await self.bot.send_message(ctx.message.author, "I need the `Embed links` permission on `{}` to embed your message".format(ctx.message.server))
+            await self.bot.send_message(ctx.message.author,
+                                        "I need the `Embed links` permission on `{}`"
+                                        "to embed your message"
+                                        "".format(ctx.message.server))
 
     @commands.command(hidden=True, pass_context=True, no_pm=True)
     @checks.is_owner()
@@ -53,24 +63,28 @@ class EmbedNotification:
         and it does not face the same restrictions as embednotifications."""
 
         color = ''.join([choice('0123456789ABCDEF') for x in range(5)])
-        color = int(colour, 16)
-        await ctx.invoke(self.embednotification, text=text, color=str(colour), ignore_deletion=True)
+        color = int(color, 16)
+        await ctx.invoke(self.embednotification, text=text, color=str(color), ignore_deletion=True)
 
         try:
             await self.bot.delete_message(ctx.message)
         except:
-            await self.bot.send_message(ctx.message.server.get_member(self.bot.settings.owner), 'Your selfbot/This userbot is able to be used by others.\nThis is breaking Discords TOS and can be punished by them.\nThis messagte was send my embednotification.py')
+            await self.bot.send_message(ctx.message.server.get_member(self.bot.settings.owner),
+                                        'Your selfbot/This userbot is able to be used by others.\n'
+                                        'This is breaking Discords TOS and can be punished by them.'
+                                        '\nThis messagte was send my embednotification.py')
             return
 
-        if not normaltext:
-            normaltext = u"\u2063" * randint(1, 10) # Generating a random number for a empty embed
+        normaltext = u"\u2063" * randint(1, 10) # Generating a random number for a empty embed
 
         data = discord.Embed(description=str(text), colour=discord.Colour(value=color))
 
         try:
             await self.bot.say(normaltext, embed=data)
         except:
-            await self.bot.send_message(ctx.message.author, "I need the `Embed links` permission on `{}` to embed your message".format(ctx.message.server))
+            await self.bot.send_message(ctx.message.author,
+                                        "I need the `Embed links` permission on `{}`"
+                                        " to embed your message".format(ctx.message.server))
 
 def setup(bot):
     bot.add_cog(EmbedNotification(bot))
