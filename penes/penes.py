7c6e5dd1487c408508dd209534a094b2a561752d
Jan200101 <sentrycraft123@gmail.com>
2017-08-26 23:38:33 +0200

    Added penes, a penis cog using hash

---
+++ penes/penes.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+import random
+import hashlib
+
+class Penes:
+    """Like Penis but using Hash"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def penes(self, user : discord.Member):
+        """Detects the users internet penis length"""
+        pl = 0
+        for x in str(int(hashlib.sha1(user.id.encode('utf-8')).hexdigest(), 16) % (10 ** 8)):
+            pl += int(x)
+        pl -= len(user.id)
+        p = "8" + "="*pl + "D"
+        await self.bot.say("Size: " + p)
+
+def setup(bot):
+    bot.add_cog(Penes(bot))
