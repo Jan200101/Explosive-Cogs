3300f0a28d774b1bdffdbd866baacd0fcc634e0e
Caleb Johnson <me@calebj.io>
2017-09-22 09:44:15 -0400

    Work with color codes with hex digits, readability

--- embednotification/embednotification.py
+++ embednotification/embednotification.py
@@ -14,35 +14,32 @@ class EmbedNotification:
     async def embednotification(self, ctx, text: str, color: str = '000000'):
         """Send a embed useful for announcements"""
 
-        if ctx.message.server.me.bot:
-            try:
-                await self.bot.delete_message(ctx.message)
-            except:
-                await self.bot.send_message(ctx.message.author,
-                                            'I need the `Manage Messages` permission on `{}`'
-                                            'to delete your message before it gets embeded.\n'
-                                            'This is done to ensure the bots embed is in'
-                                            'the same position your command was in.'
-                                            ''.format(ctx.message.channel.mention))
-                return
-        else:
-            try:
-                await self.bot.delete_message(ctx.message)
-            except:
-                await self.bot.send_message(ctx.message.channel,
-                                            'Your selfbot/This userbot is '
-                                            'able to be used by others.\n'
-                                            'This is breaking Discords TOS and'
-                                            ' can be punished by them.\n'
-                                            'This messagte was send by embednotification.py')
+        try:
+            await self.bot.delete_message(ctx.message)
+        except:
+            if ctx.message.server.me.bot:
+                dest = ctx.message.author
+                msg = ('I need the `Manage Messages` permission on `{}` '
+                       'to delete your message before it gets embeded.\n'
+                       'This is done to ensure the bots embed is in '
+                       'the same position your command was in.'
+                       ).format(ctx.message.channel.mention)
+            else:
+                dest = ctx.message.channel
+                msg = ('Your selfbot/This userbot is '
+                       'able to be used by others.\n'
+                       'This is breaking Discord\'s TOS and '
+                       'can be punished by them.\n'
+                       'This message was sent by embednotification.py')
 
-                return
-            
-        if not color.replace("#", "").replace("0x", "").isdigit():
+            await self.bot.send_message(dest, msg)
+            return
+
+        try:
+            color = color.replace("#", "").replace("0x", "")[:6]
+            color = int(color, 16)
+        except ValueError:
             color = '000000'
-            
-        color = color.replace("#", "").replace("0x", "")[:6]
-        color = int(color, 16)
 
         normaltext = u"\u2063" * randint(1, 10) # Generating a random number for a empty embed
 
